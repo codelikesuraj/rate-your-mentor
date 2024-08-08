d8a09e4fcd3cd6e3ffb87d8d2437f39e64e71124
Abdulbaki Suraj <codelikesuraj@gmail.com>
2024-08-08 13:29:14 +0100

    update: remove mentor as filament user

--- app/Models/Mentor.php
+++ app/Models/Mentor.php
@@ -2,13 +2,11 @@
 
 namespace App\Models;
 
-use Filament\Models\Contracts\FilamentUser;
-use Filament\Models\Contracts\HasAvatar;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
-class Mentor extends Model implements FilamentUser, HasAvatar
+class Mentor extends Model
 {
     use HasFactory;
 
@@ -17,15 +15,5 @@ class Mentor extends Model implements FilamentUser, HasAvatar
     public function votes(): HasMany
     {
         return $this->hasMany(Vote::class);
-    }
-
-    public function getFilamentAvatarUrl(): ?string
-    {
-        return $this->avatar;
-    }
-
-    public function canAccessPanel(\Filament\Panel $panel): bool
-    {
-        return true;
     }
 }
