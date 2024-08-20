88313ad574a02262489bf61ced442b76200cb783
Abdulbaki Suraj <codelikesuraj@gmail.com>
2024-08-20 21:23:06 +0100

    monitoring: treblle api monitoring

--- routes/api.php
+++ routes/api.php
@@ -7,10 +7,12 @@ use App\Http\Controllers\VoteController;
 use App\Http\Middleware\ForceJsonResponse;
 use App\Http\Middleware\RecordVoterIP;
 use Illuminate\Support\Facades\Route;
+use Treblle\Middlewares\TreblleMiddleware;
 
 Route::middleware([
     ForceJsonResponse::class,
     RecordVoterIP::class,
+    TreblleMiddleware::class
 ])->group(function () {
     Route::get("health",  ApiHealthController::class);
 
